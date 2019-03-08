30bd9aa79ff52aa7847d648baaf0fe90fae1e315
Jenny Sun <xmissj96@gmail.com>
2019-03-08 23:04:39 +0100

    Initial commit. Setup.

---
+++ Procedural_Planets/Assets/Scripts/Assignment_1/Planet.cs
@@ -0,0 +1,62 @@
+﻿/// <summary>
+/// The main Planet class
+/// Contains all methods for creating six terrain faces. For each face, the direction they're facing at will be defined.
+/// </summary>
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+	// Max is 256, because 256 squared is about the maxium amount vertices a mesh can have.
+	[Range(2, 256)]
+	public int resolution = 10;
+
+	// Save these, but hide them.
+	[SerializeField, HideInInspector]
+	MeshFilter[] meshFilters;
+	TerrainFace[] terrainFaces;
+
+	// Work in the editor, whenever we update anything.
+	private void OnValidate()
+	{
+		Initialize();
+		GenerateMesh();
+	}
+
+	private void Initialize()
+	{
+		if (meshFilters == null || meshFilters.Length == 0)
+		{
+			// For displaying the terrainFaces.
+			meshFilters = new MeshFilter[6];
+		}
+		terrainFaces = new TerrainFace[6];
+
+		Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
+		for (int i = 0; i < 6; i++)
+		{
+			if (meshFilters[i] == null)
+			{
+				GameObject meshObject = new GameObject("mesh");
+
+				// Parent it to the current transform, to keep clean hierarchy.
+				meshObject.transform.parent = transform;
+
+				// Default material.
+				meshObject.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+				meshFilters[i] = meshObject.AddComponent<MeshFilter>();
+				meshFilters[i].sharedMesh = new Mesh();
+			}
+
+			terrainFaces[i] = new TerrainFace(meshFilters[i].sharedMesh, resolution, directions[i]);
+		}
+	}
+
+	private void GenerateMesh()
+	{
+		foreach (TerrainFace face in terrainFaces)
+		{
+			face.ConstructMesh();
+		}
+	}
+
+}
---
+++ Procedural_Planets/Assets/Scripts/Others/WaterScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class WaterScript : MonoBehaviour
+{
+
+	private Mesh mesh;
+
+	private Vector3[] vertices;
+	private Vector2[] uvs;
+
+
+	// Start is called before the first frame update.
+	private void Start()
+	{
+		mesh = GetComponent<MeshFilter>().mesh;
+		vertices = mesh.vertices;
+
+		// Generates random "waves". Using Mathf.Cos to make it less like triangles and more round.
+		for (int i = 0; i < vertices.Length; i++)
+		{
+			vertices[i] += new Vector3(Mathf.Cos(Random.Range(-0.8f, 0.8f)), Mathf.Cos(Random.Range(-0.8f, 0.8f)), Mathf.Cos(Random.Range(-0.8f, 0.8f)));
+		}
+
+		// Put vertices back.
+		mesh.vertices = vertices;
+
+	}
+
+	// Update is called once per frame.
+	private void Update()
+	{ 
+
+	}
+
+	private void OnDrawGizmos()
+	{
+		if (vertices == null) return;
+		Gizmos.color = Color.red;
+		for (int i = 0; i < mesh.vertices.Length; i++)
+		{
+			Gizmos.DrawSphere(mesh.vertices[i], .05f);
+		}
+	}
+}
