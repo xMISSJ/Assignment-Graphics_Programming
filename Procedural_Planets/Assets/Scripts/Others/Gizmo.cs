fc21b52f2f2f2bd6db30aa77afa6d9ad1691b0c1
Jenny Sun <xmissj96@gmail.com>
2019-03-10 14:16:15 +0100

    Fixed rotation.

---
+++ Procedural_Planets/Assets/Scripts/Others/Gizmo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gizmo : MonoBehaviour
+{
+	public float gizmoSize = .75f;
+	public Color gizmoColour = Color.yellow;
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = gizmoColour;
+		Gizmos.DrawWireSphere(transform.position, gizmoSize);
+	}
+}
---
+++ Procedural_Planets/Assets/Scripts/Others/RotatePlanet.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlanet : MonoBehaviour
+{
+	private void Update()
+	{
+		transform.Rotate(0, 50 * Time.deltaTime, 0);
+	}
+}
